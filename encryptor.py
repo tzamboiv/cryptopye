8e4a3afb9057001a14603c361120420a013b50cb
Teddy Zamborsky <tzamborsky@uchicago.edu>
2016-07-27 10:23:32 -0500

    began encryptor file

---
+++ encryptor.py
@@ -0,0 +1,11 @@
+from pseudo_prime_check import powerMod
+from pseudo_prime_check import isPrime
+from pseudo_prime_check import isStrongPseudoprime
+import random
+import fractions
+import uuid
+import math
+from key_generator import key_maker
+from key_generator import egcd
+from key_generator import mulinv
+from key_generator import prime_maker

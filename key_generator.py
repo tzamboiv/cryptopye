15b8be14cd3c0323aae8301af5f99726912b117e
Teddy Zamborsky <tzamborsky@uchicago.edu>
2016-07-28 14:00:03 -0500

    implemented cryptosecure random number generator. optimized key generation

--- key_generator.py
+++ key_generator.py
@@ -4,13 +4,17 @@ from pseudo_prime_check import isStrongPseudoprime
 import random
 import fractions
 import uuid
+from Crypto.Util.number import bytes_to_long, long_to_bytes
+import os
+import time
 
+start = time.clock()
 
 def prime_maker(a, k):
 #k corresponds to degree of certainty that this output is prime
-    x = random.randint(10**a, 10**(a+1))
+    x = bytes_to_long(os.urandom(a))
     while isPrime(x, k) == "Composite":
-        x = random.randint(10**a, 10**(a+4))
+        x = bytes_to_long(os.urandom(a))
     return x
 
 def egcd(a, b):
@@ -29,9 +33,9 @@ def key_maker(a, k):
     q = prime_maker(a, k)
     n = p * q
     totient = (p-1)*(q-1)
-    e = random.randint(2, totient)
+    e = random.randint(2, 100000)
     while fractions.gcd(e, totient) != 1:
-        e = random.randint(2, totient)
+        e = random.randint(2, 100000)
     d = mulinv(e, totient)
     hexi = str(uuid.uuid4().hex)
     private_file_name = "Private_"+ hexi + ".txt"
@@ -40,11 +44,14 @@ def key_maker(a, k):
     private_key.write("\n")
     private_key.write("YOur private key n component =" + str(n))
     private_key.close
-    public_file_name = "Public_" + hexi + "txt"
+    public_file_name = "Public_" + hexi + ".txt"
     public_key = open(public_file_name, "w")
     public_key.write("Your public key e component =" + str(e))
     public_key.write("\n")
     public_key.write("Your public key n component =" + str(n))
     public_key.close
     return (public_file_name, private_file_name)
-print key_maker(50, 15)
+key_length = int(input("enter key length in bits"))
+print key_maker(key_length, 15)
+end = time.clock()
+print "Time to run is " + str(end - start) + " seconds"
